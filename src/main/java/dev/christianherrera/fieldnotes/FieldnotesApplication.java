0ea1bee5c40770f31e0f934472608a234eb65e9c
Christian Herrera <77373551+Chrisherr1@users.noreply.github.com>
2026-05-27 20:08:05 -0700

    initial project setup

---
+++ src/main/java/dev/christianherrera/fieldnotes/FieldnotesApplication.java
@@ -0,0 +1,13 @@
+package dev.christianherrera.fieldnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldnotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FieldnotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/christianherrera/fieldnotes/FieldnotesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.christianherrera.fieldnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FieldnotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
